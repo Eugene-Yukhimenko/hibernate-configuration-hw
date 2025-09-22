edb8cfcdafb2b13acd389c7d1df683f9e1a32791
Eugene <bormotovjenya2510@gmail.com>
2025-09-22 17:57:12 +0200

    Configured Hibernate, added Movie entity, DAO, service layer, custom exception, and annotation-based DI. Verified functionality in Main.

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,26 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+
+        Movie movie = new Movie();
+        movie.setTitle("Inception");
+        movie.setDescription("A mind-bending thriller by Christopher Nolan");
+
+        Movie savedMovie = movieService.add(movie);
+        System.out.println("Saved movie: " + savedMovie);
+
+        movieService.get(savedMovie.getId())
+                .ifPresentOrElse(
+                        m -> System.out.println("Retrieved movie: " + m),
+                        () -> System.out.println("Movie not found")
+                );
     }
 }
---
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -0,0 +1,30 @@
+package mate.academy.dao;
+
+import java.util.Optional;
+import mate.academy.model.Movie;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class MovieDaoImpl implements MovieDao {
+    @Override
+    public Movie add(Movie movie) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.save(movie);
+            transaction.commit();
+            return movie;
+        } catch (Exception e) {
+            throw new RuntimeException("Can't insert movie " + movie, e);
+        }
+    }
+
+    @Override
+    public Optional<Movie> get(Long id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return Optional.ofNullable(session.get(Movie.class, id));
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get movie by id " + id, e);
+        }
+    }
+}
